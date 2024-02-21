23c1244e5192211acae0bcfd4b03120c53a30b48
KAMAL INNOCENT <149705620+Kamal1-inn0@users.noreply.github.com>
2024-02-21 15:04:18 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,8 @@
+//console.log(document);
+
+const registerButton = document.getElementById("registerButton");
+const registerForm = document.getElementById("registerForm");
+registerForm.addEventListener('click', () =>{
+    //alert('button clicked');
+    registerForm.style.display = "block";
+});
